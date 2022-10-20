e4462085a04106712c962cb5b77b3b80e7760343
tleclerc <tleclerc@et.intechinfo.fr>
2022-10-20 13:56:54 +0200

    initial commit

---
+++ src/modules/room/components/room-create-modal/room-create-modal.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { RoomType } from '../../room.model';
+import { RoomService } from '../../services/room.service';
+
+export class CreateRoomFormModel {
+  name: string = "";
+  type: RoomType = RoomType.Text;
+}
+
+@Component({
+  selector: 'app-room-create-modal',
+  templateUrl: './room-create-modal.component.html',
+  styleUrls: ['./room-create-modal.component.less']
+})
+export class RoomCreateModalComponent implements OnInit {
+  @ViewChild("f")
+  form: NgForm;
+
+  isVisible: boolean = false;
+  model = new CreateRoomFormModel();
+
+  constructor(private roomService: RoomService) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  async onOk() {
+    if (this.form.form.valid) {
+      // TODO invoquer la méthode create du RoomService
+      this.close();
+    }
+  }
+
+  onCancel() {
+    this.close();
+  }
+
+  open() {
+    this.isVisible = true;
+    setTimeout(() => this.form.resetForm(new CreateRoomFormModel()))
+  }
+
+  close() {
+    this.isVisible = false;
+  }
+}
