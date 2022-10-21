ccb51acfb923f0dd60925aaa1dc3851b17c5770d
tleclerc <tleclerc@et.intechinfo.fr>
2022-10-21 14:11:48 +0200

    Added error messages on non conform conpletion of the form Added working check on password being the same as the password confirmation field

--- src/modules/user/components/user-registration/user-registration.component.ts
+++ src/modules/user/components/user-registration/user-registration.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 
@@ -14,32 +14,58 @@ class UserRegistrationFormModel {
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.less']
 })
+
 export class UserRegistrationComponent implements OnInit {
-  @ViewChild("f")
-  form: NgForm;
+  
+  form: FormGroup;
+  validateForm!: UntypedFormGroup;
 
   model = new UserRegistrationFormModel();
+  
 
   constructor(
     private router: Router,
-    private userService: UserService
-  ) { }
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) { 
+
+  }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+      pwdVal: ["", [Validators.required, this.confirmationValidator]]
+    });
   }
+
+  confirmationValidator = (control: UntypedFormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.form.controls.password.value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
+
+  updateConfirmValidator(): void {
+    /** wait for refresh value */
+    Promise.resolve().then(() => this.form.controls.checkPassword.updateValueAndValidity());
+  }
+
 
   async submit() {
 
     // TODO  Vérifier que la confirmation de mot de passe correspond au mot de passe
-    if (this.form.form.invalid || this.model.password !== this.model.confirmPassword) {
-      return;
+    if (this.form.invalid || this.model.password !== this.model.confirmPassword) {
     }
 
     // TODO Enregistrer l'utilisateur via le UserService
     this.goToLogin();
+    
   }
 
   goToLogin() {
-    // TODO rediriger l'utilisateur sur "/splash/login"
+    this.router.navigate(["/splash/login"]);
   }
 }
