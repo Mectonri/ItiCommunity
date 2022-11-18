c74be3969f501f5b799764f48c53da009fcc7d53
tleclerc <tleclerc@et.intechinfo.fr>
2022-11-18 13:10:32 +0100

    Showing user profile picture next to message maybe working. To be tested

--- src/modules/feed/component/post/post.component.ts
+++ src/modules/feed/component/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { UserQueries } from 'src/modules/user/services/user.queries';
 import { Post } from '../../post.model';
 import { PostService } from '../../services/post.service';
 
@@ -11,14 +12,27 @@ export class PostComponent implements OnInit, AfterViewInit {
   @Input()
   post: Post;
 
+  public profilePicture: string | undefined;
+
   @ViewChild("anchor")
   anchor: ElementRef<HTMLDivElement>;
 
   constructor(
-    private postService: PostService
+    private postService: PostService,
+    private userQueries: UserQueries
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const userPicture = (await this.userQueries.search(this.post.createdBy.username))[0].photoUrl;
+    if( userPicture== undefined) 
+    {
+      this.profilePicture = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/434px-Unknown_person.jpg";
+    } 
+    else 
+    {
+      this.profilePicture = userPicture;
+    }
+    
   }
 
   ngAfterViewInit() {
