ae1c827d5736b74138968ad160b0afae521ff38a
tleclerc <tleclerc@et.intechinfo.fr>
2022-11-18 13:57:00 +0100

    Image embeding done

--- src/modules/feed/services/post.mapper.ts
+++ src/modules/feed/services/post.mapper.ts
@@ -8,46 +8,63 @@ export class PostMapper {
     }
   }
 
-  private parseMessage(message: string): PostMessage {
-    // TODO rajouter png jpg et gif
-    const pictureRegex = /http[s]?:\/\/.+\.(jpeg|jpg)/gmi;
+  private parseWord(word: string):  [string, MessageElement | null] {
+    
+    let messageElem: MessageElement | null = null;
+    let content = word;
 
+    const urlRegex = /(https|http)(:\/\/)(\w|\S)*/gm;
+    const pictureRegex = /http[s]?:\/\/.+\.(jpeg|jpg|png|gif)/gmi;
      // TODO mp4,wmv,flv,avi,wav
     const videoRegex = / /gmi;
-
      // TODO mp3,ogg,wav
     const audioRegex = / /gmi;
-
     const youtubeRegex = /(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi;
-    const attachements: MessageElement[] = [];
+    
 
-    const pictureMatche = pictureRegex.exec(message);
-    if (pictureMatche) {
-     // TODO ajouter un attachement de type image dans attachements
+    if (urlRegex.test(word)) {
+      content = `<a href=${word}>${word}</a>`;
+    }
+    if (pictureRegex.test(word)) {
+      messageElem = {type: 'image', url: word.match(pictureRegex)![0]};
     }
 
-    const videoMatche = videoRegex.exec(message)
+    const videoMatche = videoRegex.exec(word)
     if (videoMatche) {
      // TODO ajouter un attachement de type video dans attachements
 
     }
 
-    const audioMatche = audioRegex.exec(message)
+    const audioMatche = audioRegex.exec(word)
     if (audioMatche) {
      // TODO ajouter un attachement de type audio dans attachements
 
     }
 
-    const youtubeMatche = youtubeRegex.exec(message)
+    const youtubeMatche = youtubeRegex.exec(word)
     if (youtubeMatche) {
      // TODO ajouter un attachement de type youtube dans attachements
     }
 
+    return [content, messageElem];
+  }
+
+
+  private parseMessage(message: string): PostMessage {
+    let attachements: MessageElement[] = [];
+    let msgTextElement: MessageTextElement = {
+      type: "text",
+      content: ""
+    }
+
+    for (let word of message.split(" ")) {
+      let [ctn, msgElem] = this.parseWord(word);
+      msgTextElement.content += ctn + " ";
+      msgElem !== null? attachements.push(msgElem!) : null;
+    }
+
     return {
-      text: {
-        type: 'text',
-        content: message
-      } as MessageTextElement,
+      text: msgTextElement,
       attachements
     };
   }
